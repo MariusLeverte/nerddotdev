1fdb244dc4589e7d00b8bdc009a6855bad9b1efd
MariusLeverte <mariusdjerv@gmail.com>
2022-05-26 21:55:48 +0200

    Remove custom gradient

--- apps/web/nextUI/theme.ts
+++ apps/web/nextUI/theme.ts
@@ -6,8 +6,6 @@ const theme = createTheme({
     colors: {
       primary: "$blue500",
       myColor: "#FF0022",
-      gradient:
-        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
     },
     fonts: {
       sans: "'Montserrat', sans-serif",
