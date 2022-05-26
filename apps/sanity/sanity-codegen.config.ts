5fdb883f736a5cec61ebfea2be9fc017ea8c55c6
MariusLeverte <mariusdjerv@gmail.com>
2022-05-26 21:54:10 +0200

    Sanity codegen outputs to web

--- apps/sanity/sanity-codegen.config.ts
+++ apps/sanity/sanity-codegen.config.ts
@@ -2,7 +2,7 @@ import { SanityCodegenConfig } from "sanity-codegen";
 
 const config: SanityCodegenConfig = {
   schemaPath: "./schemas/schema.js",
-  outputPath: "./schema.ts",
+  outputPath: "../web/types/schema.ts",
 };
 
 export default config;
