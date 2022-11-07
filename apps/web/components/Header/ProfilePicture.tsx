8ec38b73c257e346087e793240d606a7203d394a
MariusLeverte <mariusdjerv@gmail.com>
2022-11-07 20:43:09 +0100

    Cleanup

--- apps/web/components/Header/ProfilePicture.tsx
+++ apps/web/components/Header/ProfilePicture.tsx
@@ -1,51 +1,6 @@
 import { Avatar } from "@nextui-org/react";
-import { auth } from "../../libs/firebase/initFirebase";
-import { signOut } from "firebase/auth";
-import { useRouter } from "next/router";
-import useUser from "../../hooks/useUser";
-import { Button } from "ui";
 import { useState } from "react";
 import { useFirebaseUser } from "../../libs/firebase/FirebaseAuthProvider";
-
-const ProfileMenu = () => {
-  const router = useRouter();
-  const { user } = useUser();
-
-  const handleAction = () => {
-    signOut(auth);
-  };
-
-  return (
-    <div className="absolute right-0 z-20 w-48 p-4 mt-2 bg-white rounded-md shadow-xl">
-      <div className="space-y-4 flex flex-col items-start">
-        <Button
-          onClick={() => router.push(`/${user?.slug}` || "")}
-          color="transparent"
-          size="xs"
-        >
-          Se profil
-        </Button>
-        <Button
-          onClick={() => router.push(`/${user?.slug}/edit` || "")}
-          color="transparent"
-          size="xs"
-        >
-          Rediger profil
-        </Button>
-        <Button
-          onClick={() => router.push(`/${user?.slug}/invite` || "")}
-          color="transparent"
-          size="xs"
-        >
-          Inviter en utvikler
-        </Button>
-        <Button onClick={handleAction} color="transparent" size="xs">
-          Logg ut
-        </Button>
-      </div>
-    </div>
-  );
-};
 
 const ProfilePicture = () => {
   const { user, loading } = useFirebaseUser();
@@ -62,7 +17,6 @@ const ProfilePicture = () => {
       >
         <Avatar src={user.photoURL || ""} size="lg" bordered color="gradient" />
       </button>
-      {open && <ProfileMenu />}
     </div>
   );
 };
--- apps/web/hooks/useUser.ts
+++ apps/web/hooks/useUser.ts
@@ -1,58 +1,13 @@
-import { atom, useAtom } from "jotai";
-import { groq } from "next-sanity";
-import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../libs/firebase/initFirebase";
-import { userWithProjectsByIdQuery } from "../libs/sanity/queries";
-import { User } from "../types/schema";
-import useSanityData from "./useSanityData";
-
-const userAtom = atom<any | undefined>(undefined);
 
 const useUser = () => {
-  const [currentUser, setCurrentUser] = useAtom(userAtom);
-
   const [user, firebaseLoading, firebaseError] = useAuthState(auth);
-  const {
-    fetchData,
-    data,
-    loading: sanityLoading,
-    error: sanityError,
-  } = useSanityData<User>(
-    {
-      query: userWithProjectsByIdQuery,
-      params: { id: user?.uid },
-    },
-    false,
-    true
-  );
-
-  useEffect(() => {
-    if (!user) return;
-    if (data) return;
-    if (sanityLoading || sanityError) return;
-    if (currentUser) return;
-
-    fetchData();
-  }, [user, data, sanityLoading, sanityError, fetchData, currentUser]);
-
-  useEffect(() => {
-    if (currentUser) return;
-    if (!data) return;
-
-    setCurrentUser({
-      ...data,
-      id: user?.uid,
-      slug: data?.slug?.current,
-      name: data?.name,
-    });
-  }, [currentUser, data, setCurrentUser, user]);
 
   return {
-    user: currentUser,
-    loading: firebaseLoading || sanityLoading,
-    error: firebaseError || sanityError,
-    refetch: fetchData,
+    user: user,
+    loading: firebaseLoading,
+    error: firebaseError,
   };
 };
 
